be8d13299c2501d1c8b2af62aadfda9c6bf49dbc
editaahn <41330862+editaahn@users.noreply.github.com>
2020-12-07 20:22:18 +0900

    apply cors permitting API to registered user

--- routes/v2.js
+++ routes/v2.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const url = require("url");
 
 const { verifyToken, apiLimiter } = require("./middlewares"); // 토큰 디코딩
 const { Domain, User, Post, Hashtag } = require("../models");
 
 const router = express.Router();
+
+router.use(async (req, res, next) => {
+  // host를 체크하여 cors 처리하는 미들웨어
+  console.log(url.parse(req.get("origin")).host);
+  const domain = await Domain.findOne({
+    where: { host: url.parse(req.get("origin")).host }, // url.parse.host: 프로토콜 제거한 호스트만 parsing
+  });
+  if (domain) {
+    cors({ origin: req.get("origin") })(req, res, next); // 호스트 일치하는 도메인이 있으면 cors를 허용하고 다음 미들웨어 호출. cors 메서드에 옵션인자 사용
+  } else {
+    next();
+  }
+});
 
 router.post("/token", apiLimiter, async (req, res) => {
   const { clientSecret } = req.body; // body에 담긴 clientSecret
@@ -36,9 +51,9 @@ router.post("/token", apiLimiter, async (req, res) => {
     );
     return res.json({
       code: 200,
-      message: '토큰이 발급되었습니다',
+      message: "토큰이 발급되었습니다",
       token,
-    })
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -53,49 +68,54 @@ router.get("/test", apiLimiter, verifyToken, (req, res) => {
   res.json(req.decoded);
 });
 
-router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
-  Post.findAll({ where: { userId: req.decoded.id }}) // 디코드된 id값과 일치하는 user의 posts를 전부 가져옴
-  .then(posts => {
-    console.log(posts);
-    res.json({
-      code: 200,
-      payload: posts, //payload에 posts를 담아서 반환
+router.get("/posts/my", apiLimiter, verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } }) // 디코드된 id값과 일치하는 user의 posts를 전부 가져옴
+    .then((posts) => {
+      console.log(posts);
+      res.json({
+        code: 200,
+        payload: posts, //payload에 posts를 담아서 반환
+      });
     })
-  })
-  .catch(error => {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: '서버 에러'
-    })
-  })
-})
-
-router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async (req, res) => {
-  try {
-    const hashtag = await Hashtag.findOne({
-      where: {
-        title: req.params.title
-      }
+    .catch((error) => {
+      console.error(error);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러",
+      });
     });
-    if (!hashtag) {
-      return res.status(404).status.json({
-        code: 404,
-        message: '검색 결과가 없습니다.'
+});
+
+router.get(
+  "/posts/hashtag/:title",
+  verifyToken,
+  apiLimiter,
+  async (req, res) => {
+    try {
+      const hashtag = await Hashtag.findOne({
+        where: {
+          title: req.params.title,
+        },
+      });
+      if (!hashtag) {
+        return res.status(404).status.json({
+          code: 404,
+          message: "검색 결과가 없습니다.",
+        });
+      }
+      const posts = await hashtag.getPosts(); // 시퀄라이즈가 생성한 메서드 getPosts를 사용
+      return res.json({
+        code: 200,
+        payload: posts,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        code: 500,
+        message: "서버 에러",
       });
     }
-    const posts = await hashtag.getPosts(); // 시퀄라이즈가 생성한 메서드 getPosts를 사용
-    return res.json({
-      code: 200,
-      payload: posts,
-    })
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: '서버 에러'
-    })
   }
-})
+);
 
 module.exports = router;
